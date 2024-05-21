d260aa4dd4c0aa4eabb2d122a08c55c9456090cd
Glauber de Sousa Fernandes <glauber.fernandes@tbmtextil.com.br>
2024-05-20 23:30:02 -0300

    Aula 15: Listar Cursos (API HTTP GET)

--- crud-spring/src/main/java/com/gftecnologia/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/gftecnologia/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.gftecnologia;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.gftecnologia.model.Course;
+import com.gftecnologia.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			
+			Course course = new Course();
+			course.setName("Angular com Spring");
+			course.setCategory("Front-end");
+			
+			courseRepository.save(course);
+		};
 	}
 
 }
---
+++ crud-spring/src/main/java/com/gftecnologia/model/Course.java
@@ -0,0 +1,81 @@
+package com.gftecnologia.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 200, nullable = false)
+	private String name;
+	
+	@Column(length = 100, nullable = false)
+	private String category;
+	
+	public Course() {
+	}
+
+	public Course(Long id, String name, String category) {
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", category=" + category + "]";
+	}
+	
+}
